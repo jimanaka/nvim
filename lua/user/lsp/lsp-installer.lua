9a2eb5308747ae46a9046aad185a8b779a8b15a6
Jake Imanaka <jake.imanaka@gmail.com>
2022-07-15 20:38:11 -1000

    fix lsp

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,41 +3,36 @@ if not status_ok then
   return
 end
 
-local servers = {
-  "sumneko_lua",
-  "cssls",
-  "html",
-  "tsserver",
-  "pyright",
-  "bashls",
-  "jsonls",
-  "yamlls",
-}
-
-lsp_installer.setup()
-
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
-
-local opts = {}
-
-for _, server in pairs(servers) do
-  opts = {
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+  local opts = {
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = require("user.lsp.handlers").capabilities,
   }
 
-  if server == "sumneko_lua" then
-    local sumneko_opts = require "user.lsp.settings.sumneko_lua"
+  if server.name == "jsonls" then
+    local jsonls_opts = require("user.lsp.settings.jsonls")
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
+
+  if server.name == "sumneko_lua" then
+    local sumneko_opts = require("user.lsp.settings.sumneko_lua")
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
 
-  if server == "pyright" then
-    local pyright_opts = require "user.lsp.settings.pyright"
+  if server.name == "pyright" then
+    local pyright_opts = require("user.lsp.settings.pyright")
     opts = vim.tbl_deep_extend("force", pyright_opts, opts)
   end
 
-  lspconfig[server].setup(opts)
-end
+  if server == "rust-analyzer" then
+    local rustanalyzer_opts = require "user.lsp.settings.rust-analyzer"
+    opts = vim.tbl_deep_extend("force", rustanalyzer_opts, opts)
+  end
+
+
+  -- This setup() function is exactly the same as lspconfig's setup function.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
+end)
