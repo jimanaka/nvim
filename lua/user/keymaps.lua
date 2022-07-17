0f24e87812e0996b693ef5d3e47769d7cd48b262
Jake Imanaka <jake.imanaka@gmail.com>
2022-07-17 08:21:22 -1000

    move lines

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -51,6 +51,18 @@ keymap("i", "kj", "<ESC>", opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- Move text up and down (Alt keybinds may only work on some terminals)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
 -- Plugins --
 
 -- NvimTree
